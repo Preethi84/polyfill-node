0fc709847a931d940e79d2532608ed423399b0ff
Preethi84 <preethi18.c@gmail.com>
2024-04-17 20:24:26 +0530

    first commit

---
+++ promises_es5.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var greet = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve("Welcome to GeeksforGeeks");
+    document.getElementById("one").innerHTML = "Poly means it could be solved using a wide range of techniques, it wasn't limited \n        to just being done using Javascript, and fill would fill the gap in the browsers where the technology was needed";
+    document.getElementById("two").innerHTML = "A polyfill is a piece of code that adds functionality to older browsers that have \n        incompatibility issues.";
+    document.getElementById("three").innerHTML = "In older browsers, Promises, Array.from, Array.includes, Array.of, Map,\n        Set, Symbol, object.values, etc require polyfill support by explicitly defining the functions";
+    document.getElementById("four").innerHTML = "Promises represent uncompleted operations";
+  }, 3000);
+});
+greet.then(function (msg) {
+  console.log(msg);
+});
